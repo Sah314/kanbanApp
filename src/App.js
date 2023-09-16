d9994efb37addc6dca136106bf4ab567f6e63600
sah_314 <khadayatesahil@gmail.com>
2023-09-16 16:49:01 +0530

    Created and made changes in App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Canvas from './components/Canvas';
 function App() {
   const items=[
     {title:"Group-1",item:["A","B","C"]},
-    {title:"Group-2",item:["D","E"]}
+    {title:"Group-2",item:["D","E"]},
+    {title:"Group-3",item:["D","E"]}
 ]
   return (
     <div className="App">
